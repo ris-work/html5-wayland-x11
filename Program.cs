b13ba21cd91d02c7a5da08a2ed272b80a07ebabc
Rishi <oss@rishikeshan.com>
2026-05-05 08:44:51 +0000

    Fix potential XSS
    FossilOrigin-Name: fb31f300982a768731da3de48bb076aa09ac9b74b631d3dec52c242ecfd7bf4d

--- Program.cs
+++ Program.cs
@@ -1530,6 +1530,7 @@ RequestDelegate WsHandler = async (HttpContext context) =>
 app.MapGet("/launch", (HttpContext context) =>
 {
     System.Console.WriteLine("[launch] Launch endpoint hit...");
+    string E(string s) => System.Net.WebUtility.HtmlEncode(s);
     var query = context.Request.Query;
 
     // Helper to safely get query values for pre-filling
@@ -1591,12 +1592,12 @@ app.MapGet("/launch", (HttpContext context) =>
 
             <div class='form-group'>
                 <label for='u'>Basic Auth User</label>
-                <input type='text' id='u' name='u' placeholder='Username' value='{userVal}'>
+                <input type='text' id='u' name='u' placeholder='Username' value='{E(userVal)}'>
             </div>
 
             <div class='form-group'>
                 <label for='p'>Basic Auth Pass</label>
-                <input type='password' id='p' name='p' placeholder='Password' value='{passVal}'>
+                <input type='password' id='p' name='p' placeholder='Password' value='{E(passVal)}'>
             </div>
 
             <div class='form-group'>
